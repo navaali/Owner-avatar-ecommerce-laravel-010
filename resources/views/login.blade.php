2d9b694d05be04146ddca41e60a57e8a0c85c304
navaali <90944758+navaali@users.noreply.github.com>
2025-06-11 20:54:20 +0330

    login done. after you login and refresh the page you cant see the login page again

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,25 +1,24 @@
 @extends('master')
 @section("content")
 
-
-
 <div class="container custom-login">
   <div class="row justify-content-center">
     <div class="col-md-6 col-lg-4">
-      <form>
+      <form action="login" method="POST">
         <div class="mb-3 row">
+            @csrf
           <label for="inputEmail3" class="col-sm-3 col-form-label">Email</label>
           <div class="col-sm-9">
-            <input type="email" class="form-control" id="inputEmail3">
+            <input type="email" name="email" class="form-control" id="inputEmail3">
           </div>
         </div>
         <div class="mb-3 row">
           <label for="inputPassword3" class="col-sm-3 col-form-label">Password</label>
           <div class="col-sm-9">
-            <input type="password" class="form-control" id="inputPassword3">
+            <input type="password" name="password" class="form-control" id="inputPassword3">
           </div>
         </div>
-        <button type="submit" class="btn btn-primary">Sign in</button>
+        <button type="submit" class="btn btn-primary">Login</button>
       </form>
     </div>
   </div>
